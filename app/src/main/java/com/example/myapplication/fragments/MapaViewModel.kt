3c9e31343b31154fc26f92a834321695eb77c60d
PEI-user17 <lucasrebasa02@hotmail.com>
2023-09-28 21:00:10 -0300

    Se muestran datos de ubicacion en tiempo real pade paseador cuando el dueño ya tiene un paseo asignado

--- app/src/main/java/com/example/myapplication/fragments/MapaViewModel.kt
+++ app/src/main/java/com/example/myapplication/fragments/MapaViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.example.myapplication.R
 import com.example.myapplication.entities.Paseador
 import com.example.myapplication.repository.PaseadorRepository
+import com.example.myapplication.repository.PaseoRepository
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
@@ -18,7 +19,7 @@ import com.google.firebase.database.ValueEventListener
 
 class MapaViewModel : ViewModel() {
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val paseadorRepository: PaseadorRepository = PaseadorRepository()
+    private val paseoRepository = PaseoRepository.getInstance()
     val marcadores: MutableList<Marker?> = mutableListOf()
     fun getPaseadoresLocation(gMap: GoogleMap, paseador: Paseador) {
         val locationRef = database.getReference("paseadores")
@@ -30,7 +31,7 @@ class MapaViewModel : ViewModel() {
                     snapshot.children.first().child("location").child("latitude").value
                 val userLongitude =
                     snapshot.children.first().child("location").child("longitude").value
-                Log.d("UBICACION", userLongitude.toString() + " " + userLatitude.toString())
+
                 if (userLatitude != null && userLongitude != null) {
                     val userLatLng = LatLng(userLatitude as Double, userLongitude as Double)
 
@@ -53,6 +54,41 @@ class MapaViewModel : ViewModel() {
         })
     }
 
+    fun getPaseoUser(gMap: GoogleMap,dataSnapshot: DataSnapshot) {
+        val mapData = dataSnapshot.value as HashMap<*, *>
+        Log.d("MAP DATA", mapData.toString())
+        val mapPaseo = mapData.get(mapData.keys.first()) as HashMap<*, *>
+        val dniUser = (mapPaseo.get("user") as HashMap<*, *>).get("dni") as String
+
+        paseoRepository.getPaseoUserRef(dniUser).addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val mapDataSnap = snapshot.value as HashMap<*, *>
+                val mapPaseoSnap = mapDataSnap.get(mapDataSnap.keys.first()) as HashMap<*, *>
+                val paseadorMap = mapPaseoSnap.get("paseador") as HashMap<*, *>
+                val paseadorDni = paseadorMap.get("dni").toString()
+                val locationMap = paseadorMap.get("location") as HashMap<*, *>
+                val latitude = locationMap.get("latitude").toString().toDouble()
+                val longitude = locationMap.get("longitude").toString().toDouble()
+                Log.d("PASO POR ACA", "PASO POR ACA")
+                if (latitude != null && longitude != null) {
+                    val marcador = marcadores.find { it?.title.equals(paseadorDni) }
+                    if (marcador != null) {
+                        Log.d("PASO POR ACA", "PASO POR $latitude $longitude")
+                        marcador.position = LatLng(latitude, longitude)
+                    } else {
+                        addMarcador(gMap, LatLng(latitude, longitude), paseadorDni)
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
     fun addMarcador(gMap: GoogleMap, latLng: LatLng, dni: String) {
         val marker = gMap.addMarker(
             MarkerOptions()
