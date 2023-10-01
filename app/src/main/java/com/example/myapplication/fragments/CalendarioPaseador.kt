feca2a85735c12e5ccdbb03e182436eccf046848
PEI-user17 <lucasrebasa02@hotmail.com>
2023-10-01 14:38:57 -0300

    Se agrega filtro para no tener paseos en el mismo horario

--- app/src/main/java/com/example/myapplication/fragments/CalendarioPaseador.kt
+++ app/src/main/java/com/example/myapplication/fragments/CalendarioPaseador.kt
@@ -3,10 +3,12 @@ package com.example.myapplication.fragments
 import android.app.AlertDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CalendarView
@@ -29,6 +31,9 @@ class CalendarioPaseador : Fragment() {
     lateinit var selectedDate: TextView
     lateinit var btnConfirm : Button
     lateinit var spinner : Spinner
+    lateinit var adaptador: ArrayAdapter<String>
+    val programadoRepository = PaseoProgramadoRepository.getInstance()
+    val horarios = mutableListOf("10:00hs", "11:00hs", "12:00hs")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +45,7 @@ class CalendarioPaseador : Fragment() {
         btnConfirm = v.findViewById(R.id.btnConfirm)
 
         spinner = v.findViewById(R.id.listaHorarios)
-        val horarios = listOf("10:00hs", "11:00hs", "12:00hs")
-        val adaptador = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, horarios)
+        adaptador = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, horarios)
         adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adaptador
 
@@ -62,15 +66,28 @@ class CalendarioPaseador : Fragment() {
         calendar.maxDate = maxSelectableDateInMillis
 
         calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            val selectedTime = spinner.selectedItem.toString()
             val selectedDateCalendar =
                 "$dayOfMonth/${month + 1}/$year" // Suma 1 al mes ya que los meses comienzan en 0
             selectedDate.text = "$selectedDateCalendar"
 
-
+            programadoRepository.getPaseos { paseos ->
+                val filtrados = paseos.filter {
+                    it.fecha.substring(0, 10).trim() == "$dayOfMonth/${(month + 1)}/${year}"
+                }
+                if (filtrados.isNotEmpty()) {
+                    val horariosAEliminar = filtrados.map { it.fecha.substring(it.fecha.length - 7)}
+                    val horariosFiltrados = horarios.filter { hora ->
+                        !horariosAEliminar.any { horaEliminar ->
+                            hora.equals(horaEliminar)
+                        }
+                    }
+                    val adaptadorActual = spinner.adapter as ArrayAdapter<String>
+                    adaptadorActual.clear()
+                    adaptadorActual.addAll(horariosFiltrados)
+                    adaptadorActual.notifyDataSetChanged()
+                }
+            }
         }
-
-
 
         btnConfirm.setOnClickListener {
             val selectedDateString = selectedDate.text.toString()
